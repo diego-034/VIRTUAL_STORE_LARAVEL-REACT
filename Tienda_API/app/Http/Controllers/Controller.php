eb18a650e2ded32d2c05049181d6a1227b964a16
diego-034 <diego-034>
2020-05-14 09:36:49 -0500

    Creación de standar de respuesta

--- Tienda_API/app/Http/Controllers/Controller.php
+++ Tienda_API/app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function SendResponse($result, $message)
+    {
+        $response = [
+            "success" => true,
+            "data" => $result,
+            "message" => $message
+        ];
+        return response()->json($response, 200);
+    }
+    
+    public function SendError($error, $errorMessage = [], $code = 404)
+    {
+        $response = [
+            "success" => false,
+            "error" => $error,
+            "message" => $errorMessage
+        ];
+        return response()->json($response, $code);
+    }
 }
--- Tienda_API/app/Producto.php
+++ Tienda_API/app/Producto.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     protected $primaryKey = 'SKU';
-    protected $fillable  = ['Nombre','Descripcion','Valor','Imagen','IdTienda   '];
+    protected $fillable  = ['Nombre','Descripcion','Valor','Imagen','IdTienda'];
 }

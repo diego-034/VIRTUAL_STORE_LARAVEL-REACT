45dad5e445a6cd9a2c0074ed862de5e3bc6d6a4e
diego-034 <diego-034>
2020-05-14 07:10:36 -0500

    Creación de migrations,controller,model de Tienda

---
+++ Tienda_API/database/factories/TiendaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tienda;
+use Faker\Generator as Faker;
+
+$factory->define(Tienda::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
